613bdbeb5ae1534969498e8e9676caa296181722
Sean Knox <knoxville@gmail.com>
2013-05-20 16:23:13 -0700

    Use neo/vim-config with vundle

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,32 +16,48 @@ class vim {
   # Install mercurial since the vim brew package don't satisfy the requirement
   package { 'mercurial': }
 
-  file { ["/Users/${::boxen_user}/.vim",
-    "/Users/${::boxen_user}/.vim/autoload",
-    "/Users/${::boxen_user}/.vim/bundle"]:
-    ensure  => directory,
-    recurse => true,
+  # file { ["/Users/${::boxen_user}/.vim",
+  #   "/Users/${::boxen_user}/.vim/autoload",
+  #   "/Users/${::boxen_user}/.vim/bundle"]:
+  #   ensure  => directory,
+  #   recurse => true,
+  # }
+
+  repository { "/Users/${::boxen_user}/vim-config":
+    source => 'neo/vim-config'
+  }
+  repository { "/Users/${::boxen_user}/vim-config/.vim/vundle.git":
+    source => 'gmarik/vundle',
+    require => Repository["/Users/${::boxen_user}/vim-config"]
   }
 
-  repository { "/Users/${::boxen_user}/.vim/vim-pathogen":
-    source => 'tpope/vim-pathogen'
-  }
-
-  file { "/Users/${::boxen_user}/.vim/autoload/pathogen.vim":
-    target  => "/Users/${::boxen_user}/.vim/vim-pathogen/autoload/pathogen.vim",
+  file { "/Users/${::boxen_user}/.vim":
+    audit   => content,
+    target  => "/Users/${::boxen_user}/vim-config/.vim",
     require => [
-      File["/Users/${::boxen_user}/.vim"],
-      File["/Users/${::boxen_user}/.vim/autoload"],
-      File["/Users/${::boxen_user}/.vim/bundle"],
-      Repository["/Users/${::boxen_user}/.vim/vim-pathogen"]
+      # File["/Users/${::boxen_user}/.vim"],
+      # File["/Users/${::boxen_user}/.vim/autoload"],
+      # File["/Users/${::boxen_user}/.vim/bundle"],
+        Repository["/Users/${::boxen_user}/vim-config"],
+      # Repository["/Users/${::boxen_user}/vim-config/.vim/vundle.git"]
     ]
   }
 
-  # Install pathogen into .vimrc
-  file_line { 'load_pathogen':
-    ensure  => present,
-    line    => 'execute pathogen#infect()',
-    path    => "/Users/${::boxen_user}/.vimrc",
-    require => File["/Users/${::boxen_user}/.vimrc"]
+  # Install vundle
+  exec { "load_vundle":
+    command => 'vim +BundleInstall +qall 2>&1 >/dev/null',
+    require => [
+      Repository["/Users/${::boxen_user}/vim-config/.vim/vundle.git"]
+    ],
+    subscribe => File['/Users/${::boxen_user}/.vim']
   }
+
+  # Install pathogen into .vimrc
+  # file_line { 'load_pathogen':
+  #   ensure  => present,
+  #   line    => 'execute pathogen#infect()',
+  #   path    => "/Users/${::boxen_user}/.vimrc",
+  #   require => File["/Users/${::boxen_user}/.vimrc"]
+  # }
 }
+
